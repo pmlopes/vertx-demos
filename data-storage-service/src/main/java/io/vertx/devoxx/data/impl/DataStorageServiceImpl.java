0afb7e655f611f3c6b338533e989ec6b44f995e6
Clement Escoffier <clement.escoffier@gmail.com>
2015-11-04 09:07:01 +0100

    Add to log in the data storage (use for the hawkular)

--- data-storage-service/src/main/java/io/vertx/devoxx/data/impl/DataStorageServiceImpl.java
+++ data-storage-service/src/main/java/io/vertx/devoxx/data/impl/DataStorageServiceImpl.java
@@ -10,6 +10,7 @@ import io.vertx.devoxx.data.Place;
 import io.vertx.ext.mongo.MongoClient;
 
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 /**
@@ -18,10 +19,14 @@ import java.util.stream.Collectors;
 public class DataStorageServiceImpl implements DataStorageService {
 
   public static final String COLLECTION = "places";
+  public static final Logger LOGGER = Logger.getLogger("Data Storage Service");
   private final MongoClient mongo;
+  private final Vertx vertx;
 
   public DataStorageServiceImpl(Vertx vertx, JsonObject config) {
     this.mongo = MongoClient.createShared(vertx, config, "places");
+    this.vertx = vertx;
+    LOGGER.info("Data Storage Service instantiated");
   }
 
   @Override
@@ -29,26 +34,37 @@ public class DataStorageServiceImpl implements DataStorageService {
     /**
      * To be implemented.
      */
+    long begin = System.currentTimeMillis();
     mongo.find(COLLECTION, new JsonObject(), ar -> {
       if (ar.failed()) {
         resultHandler.handle(Future.failedFuture(ar.cause()));
       } else {
         List<Place> places = ar.result().stream().map(Place::new).collect(Collectors.toList());
+        LOGGER.info("Retrieving places from MongoDB : " + places.size());
         resultHandler.handle(Future.succeededFuture(places));
+        long time = System.currentTimeMillis() - begin;
+        report(time);
       }
     });
   }
 
   @Override
   public void getPlacesForCategory(String category, Handler<AsyncResult<List<Place>>> resultHandler) {
+    long begin = System.currentTimeMillis();
     mongo.find(COLLECTION, new JsonObject().put("category", category), ar -> {
       if (ar.failed()) {
         resultHandler.handle(Future.failedFuture(ar.cause()));
       } else {
         List<Place> places = ar.result().stream().map(Place::new).collect(Collectors.toList());
         resultHandler.handle(Future.succeededFuture(places));
+        long time = System.currentTimeMillis() - begin;
+        report(time);
       }
     });
+  }
+
+  private void report(long time) {
+    vertx.eventBus().send("metrics", new JsonObject().put("source", "mongo.query").put("value", time));
   }
 
   @Override
